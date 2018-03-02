6d04dd1b45a1a05a9215a43be01d9bfb1b51f872
Hinrik <hinriksnaer@gmail.com>
2018-03-02 10:52:55 +0000

    pushed everything

--- api.js
+++ api.js
@@ -10,10 +10,44 @@ const {
 
 const router = express.Router();
 
-function catchErrors(fn) {
-  return (req, res, next) => fn(req, res, next).catch(next);
+function getRequest(req, res) {
+  const data = req.body;
+  create(data).then((x => res.send(JSON.stringify(x))));
+}
+
+function lookup(req, res) {
+  const { id } = req.params.id;
+  readOne(id).then((x => res.send(JSON.stringify(x))));
+}
+
+function findall(req, res) {
+  readAll().then(x => res.send(JSON.stringify(x)));
+}
+
+function remove(req, res) {
+  const { id } = req.params.id;
+  del(id).then(((x) => {
+    if (x !== null) {
+      res.send(x);
+    } else {
+      res.status(202);
+      res.send();
+    }
+  }));
+}
+
+function alter(req, res) {
+  const data = req.body;
+  const { id } = req.params.id;
+  update(id, data).then(x => res.send(JSON.stringify(x)));
 }
 
 /* todo útfæra api */
+router.post('/', getRequest);
+router.get('/:id', lookup);
+router.get('/', findall);
+router.delete('/:id', remove);
+router.put('/:id', alter);
+
 
 module.exports = router;
--- notes.js
+++ notes.js
@@ -1,6 +1,33 @@
-/* todo sækja pakka sem vantar  */
+const { Client } = require('pg');
+const xss = require('xss');
+const validator = require('validator');
 
 const connectionString = process.env.DATABASE_URL;
+
+function validate(title, text, datetime) {
+  const returnList = [];
+
+  if (!typeof title === 'string' || !validator.isLength(title, { min: 1, max: 255 })) {
+    returnList.push({
+      field: 'title',
+      message: 'Title must be a string of length 1 to 255 character',
+    });
+  }
+  if (typeof text !== 'string') {
+    returnList.push({
+      field: 'text',
+      message: 'Text must be a string',
+    });
+  }
+
+  if (!validator.toDate(datetime)) {
+    returnList.push({
+      field: 'datetime',
+      message: 'Datetime must be ISO 8601 date',
+    });
+  }
+  return returnList;
+}
 
 /**
  * Create a note asynchronously.
@@ -13,7 +40,22 @@ const connectionString = process.env.DATABASE_URL;
  * @returns {Promise} Promise representing the object result of creating the note
  */
 async function create({ title, text, datetime } = {}) {
-  /* todo útfæra */
+  const data = [xss(datetime), xss(title), xss(text)];
+
+  const returnObject = validate(title, text, datetime);
+
+  if (returnObject.length <= 0) {
+    const client = new Client({ connectionString });
+    await client.connect();
+    const resolve = await client.query('INSERT INTO public.notes(datetime, title, text) VALUES ($1, $2, $3) RETURNING *', data);
+    return {
+      id: resolve.rows[0].id,
+      title: resolve.rows[0].title,
+      text: resolve.rows[0].text,
+      datetime: resolve.rows[0].datetime,
+    };
+  }
+  return returnObject;
 }
 
 /**
@@ -22,7 +64,19 @@ async function create({ title, text, datetime } = {}) {
  * @returns {Promise} Promise representing an array of all note objects
  */
 async function readAll() {
-  /* todo útfæra */
+  const returnList = [];
+  const client = new Client({ connectionString });
+  await client.connect();
+  const resolve = await client.query('SELECT * FROM notes;');
+  for (let i = 0; i < resolve.rows.length; i++) { // eslint-disable-line
+    returnList.push({
+      id: resolve.rows[i].id,
+      title: resolve.rows[i].title,
+      text: resolve.rows[i].text,
+      datetime: resolve.rows[i].datetime,
+    });
+  }
+  return returnList;
 }
 
 /**
@@ -33,7 +87,21 @@ async function readAll() {
  * @returns {Promise} Promise representing the note object or null if not found
  */
 async function readOne(id) {
-  /* todo útfæra */
+  const client = new Client({ connectionString });
+  await client.connect();
+  try {
+    const resolve = await client.query('SELECT * FROM notes where id = $1', [id]);
+    return {
+      id: resolve.rows[0].id,
+      title: resolve.rows[0].title,
+      text: resolve.rows[0].text,
+      datetime: resolve.rows[0].datetime,
+    };
+  } catch (err) {
+    return {
+      error: 'Note not found',
+    };
+  }
 }
 
 /**
@@ -48,7 +116,27 @@ async function readOne(id) {
  * @returns {Promise} Promise representing the object result of creating the note
  */
 async function update(id, { title, text, datetime } = {}) {
-  /* todo útfæra */
+  const data = [xss(datetime), xss(title), xss(text)];
+  const returnObject = validate(title, text, datetime);
+  if (returnObject.length <= 0) {
+    try {
+      const client = new Client({ connectionString });
+      await client.connect();
+      const resolve = await client.query('UPDATE notes SET datetime = $2, title = $3, text = $4 WHERE id = $1 RETURNING *;', [id, data[0], data[1], data[2]]);
+      return {
+        id: resolve.rows[0].id,
+        title: resolve.rows[0].title,
+        text: resolve.rows[0].text,
+        datetime: resolve.rows[0].datetime,
+      };
+    } catch (err) {
+      return {
+        error: 'Note not found',
+      };
+    }
+  } else {
+    return returnObject;
+  }
 }
 
 /**
@@ -59,7 +147,15 @@ async function update(id, { title, text, datetime } = {}) {
  * @returns {Promise} Promise representing the boolean result of creating the note
  */
 async function del(id) {
-  /* todo útfæra */
+  const client = new Client({ connectionString });
+  await client.connect();
+  const result = await client.query('DELETE FROM notes WHERE id = $1', [id]);
+  if (result.rowCount !== 1) {
+    return {
+      error: 'Note not found',
+    };
+  }
+  return null;
 }
 
 module.exports = {
